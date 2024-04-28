dcf372547cb174928170d039ee3f2786be623e01
ice breaker <1324318532@qq.com>
2024-04-28 23:16:41 +0800

    chore: flat config

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// import antfu from '@antfu/eslint-config'
+
+export default [
+    {
+        rules: {
+            // override/add rules settings here, such as:
+            // 'vue/no-unused-vars': 'error'
+        }
+    }
+]
